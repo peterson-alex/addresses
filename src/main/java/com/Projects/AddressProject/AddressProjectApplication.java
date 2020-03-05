63538f5868e7e56afaafe952fea8f93778798fb1
Alex Peterson <alexpeterson80@gmail.com>
2020-03-04 18:19:19 -0800

    Initial commit.

---
+++ src/main/java/com/Projects/AddressProject/AddressProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Projects.AddressProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Projects/AddressProject/AddressProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projects.AddressProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
