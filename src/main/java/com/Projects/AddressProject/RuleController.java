c2d314dbab41451e3b00514886843cd9fc076a99
Alex Peterson <alexpeterson80@gmail.com>
2020-03-04 21:27:58 -0800

    Add Rule Model validation to RuleController.

--- src/main/java/com/Projects/AddressProject/RuleController.java
+++ src/main/java/com/Projects/AddressProject/RuleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.*;
 
 @RestController
 public class RuleController {
@@ -46,7 +47,66 @@ public class RuleController {
     @PostMapping("api/rule")
     public RuleModel createRule(@RequestBody RuleModel ruleModel)
     {
-        ruleRepository.save(ruleModel);
+        if (ruleModelIsValid(ruleModel)){
+            return ruleRepository.save(ruleModel);
+        }
         return ruleModel;
     }
+
+    // Posts a list of rules to the database
+    @PostMapping("api/rules")
+    public List<RuleModel> createManyRules(@RequestBody List<RuleModel> ruleModelList) {
+        return ruleRepository.saveAll(ruleModelList);
+    }
+
+    // Address Rule checker. Rules must be valid to be inserted
+    // into database.
+    private boolean ruleModelIsValid(RuleModel ruleModel) {
+        // TODO - check that country, iso2, iso3 consistent
+
+        // check that at least one field in ruleModel.addressfields
+        if (ruleModel.AddressFieldList.size() < 1) {
+            return false;
+        }
+
+        // check name, format, and required fields
+        int numRequiredFields = 0;
+        for (RuleModel.AddressFieldModel field : ruleModel.AddressFieldList) {
+
+            // check that name is not null
+            if (field.Name == null) {
+                return false;
+            }
+
+            // check that name is not blank
+            if (field.Name.isBlank()) {
+                return false;
+            }
+
+            // check that name is not empty
+            if (field.Name.isEmpty()) {
+                return false;
+            }
+
+            // check if regex is valid
+            try {
+                Pattern.compile(field.Format);
+            } catch (PatternSyntaxException ex) {
+                return false;
+            }
+
+            if (field.required) {
+                numRequiredFields++;
+            }
+        }
+
+        // at least one field must be required
+        if (numRequiredFields < 1) {
+            return false;
+        }
+
+        // inputs of rule model are valid
+        return true;
+    }
+
 }
