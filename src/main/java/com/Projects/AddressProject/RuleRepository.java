320ef62cd84928d9214b07c2f203fb47b091ff9e
Alex Peterson <alexpeterson80@gmail.com>
2020-03-04 18:39:29 -0800

    Modify RuleRepository to be Restful.

--- src/main/java/com/Projects/AddressProject/RuleRepository.java
+++ src/main/java/com/Projects/AddressProject/RuleRepository.java
@@ -1,12 +1,14 @@
 package com.Projects.AddressProject;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
-import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "rule", path = "rule")
 public interface RuleRepository extends MongoRepository<RuleModel, String> {
 
-    public RuleModel findByCountryName(String countryName);
+    public RuleModel findByCountryName(@Param("country") String country);
     public List<RuleModel> getAllRules(); // retrieves all rules from database
 
 }
