ad69964ee6aa72472a593e83451e87bf8ba65c90
Alex Peterson <alexpeterson80@gmail.com>
2020-03-04 18:32:00 -0800

    Add RuleRepository to AddressProjectApplication.

--- src/main/java/com/Projects/AddressProject/AddressProjectApplication.java
+++ src/main/java/com/Projects/AddressProject/AddressProjectApplication.java
@@ -1,10 +1,14 @@
 package com.Projects.AddressProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressProjectApplication {
+
+	@Autowired
+	private RuleRepository ruleRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressProjectApplication.class, args);
