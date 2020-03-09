bb503aa958c34ba9722eb8f22f825a8a72886358
Alex Peterson <alexpeterson80@gmail.com>
2020-03-08 17:14:31 -0700

    Implement addressIsValid method.

--- src/main/java/com/Projects/AddressProject/AddressController.java
+++ src/main/java/com/Projects/AddressProject/AddressController.java
@@ -9,6 +9,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.*;
 
 @RestController
 @RequestMapping("/api")
@@ -68,10 +69,81 @@ public class AddressController {
         return addressModel;
     }
 
-    //
+    // Ensure that address is valid according to address rules
     private boolean addressIsValid(AddressModel addressModel, RuleModel ruleModel) {
-        // TODO - need to implement this method
+
+        // check country
+        if (!addressModel.country.equals(ruleModel.country)) {
+            return false;
+        }
+
+        // check iso2
+        if (addressModel.ISO2.equals((ruleModel.ISO2))) {
+            return false;
+        }
+
+        // check iso3
+        if (addressModel.ISO3.equals(ruleModel.ISO3)) {
+            return false;
+        }
+
+        // loop through address fields of address model and validate each
+        for (Map.Entry<String, Object> field : addressModel.Address.entrySet()) {
+
+            // check if field name in rule model
+            if (!fieldInRuleModel(field.getKey(), ruleModel)) {
+                return false;
+            }
+
+            // get regex pattern
+            String regex = getFieldRegexPattern(field.getKey(), ruleModel);
+
+            // check if field matches regex in rule model
+            if (!Pattern.compile(regex).matcher((String)field.getValue()).matches()) {
+                return false;
+            }
+        }
+
+        // loop through address fields of rule model. If field required,
+        // check that field is in address model and not null
+        for (AddressFieldModel fieldModel : ruleModel.AddressFieldList) {
+            if (fieldModel.required) {
+                if (!addressModel.Address.containsKey(fieldModel.name)){
+                    return false;
+                }
+
+                // value must be present - cannot be null
+                if (addressModel.Address.get(fieldModel.name) == null) {
+                    return false;
+                }
+            }
+        }
+
+        // all checks passed
         return true;
+    }
+
+    // check if field name is present in rule model
+    private boolean fieldInRuleModel(String field, RuleModel ruleModel) {
+
+        for (AddressFieldModel fieldModel : ruleModel.AddressFieldList) {
+            if (fieldModel.name.equals(field)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // returns the regex pattern for the given field
+    private String getFieldRegexPattern(String field, RuleModel ruleModel) {
+        for (AddressFieldModel fieldModel : ruleModel.AddressFieldList) {
+            if (fieldModel.name.equals(field)) {
+                return fieldModel.format;
+            }
+        }
+
+        // field not in rulemodel
+        return null;
     }
 
     private String getFullAddress(AddressModel addressModel) {
