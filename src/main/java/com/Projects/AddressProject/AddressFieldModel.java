c15ac32cf48b8de19f41dd4c051c2c079bcad85c
Alex Peterson <alexpeterson80@gmail.com>
2020-03-09 14:39:28 -0700

    Add field 'discreteField' to AddressFieldModel.

--- src/main/java/com/Projects/AddressProject/AddressFieldModel.java
+++ src/main/java/com/Projects/AddressProject/AddressFieldModel.java
@@ -6,15 +6,17 @@ public class AddressFieldModel {
     // fields
     public String name; // name of field
     public String format; // regex expression
-    public boolean required; // true if field
+    public boolean discreteField; // true if field has discrete set of values ("AL", AR", ... )
+    public boolean required; // true if field required
 
     // default constructor
     public AddressFieldModel() {
     }
 
-    public AddressFieldModel(String name, String format, boolean required) {
+    public AddressFieldModel(String name, String format, boolean discreteField, boolean required) {
         this.name = name;
         this.format = format;
+        this.discreteField = discreteField;
         this.required = required;
     }
 }
