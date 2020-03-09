a45adb1ba2ac31865adc3537e835b8b2277fff8c
Alex Peterson <alexpeterson80@gmail.com>
2020-03-09 14:57:29 -0700

    Change field name to 'isDiscreteField'.

--- src/main/java/com/Projects/AddressProject/AddressFieldModel.java
+++ src/main/java/com/Projects/AddressProject/AddressFieldModel.java
@@ -6,17 +6,17 @@ public class AddressFieldModel {
     // fields
     public String name; // name of field
     public String format; // regex expression
-    public boolean discreteField; // true if field has discrete set of values ("AL", AR", ... )
+    public boolean isDiscreteField; // true if field has discrete set of values ("AL", AR", ... )
     public boolean required; // true if field required
 
     // default constructor
     public AddressFieldModel() {
     }
 
-    public AddressFieldModel(String name, String format, boolean discreteField, boolean required) {
+    public AddressFieldModel(String name, String format, boolean isDiscreteField, boolean required) {
         this.name = name;
         this.format = format;
-        this.discreteField = discreteField;
+        this.isDiscreteField = isDiscreteField;
         this.required = required;
     }
 }
